b032f8ebd719a3210a7541fbc9cd551aa2949c73
Panu Ohra-aho <panu.ohraaho@gmail.com>
2025-01-24 20:00:40 +0200

    interaction added

---
+++ Interactor.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        transform.parent.parent.GetComponent<Player>().interact_target = other.gameObject;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        transform.parent.parent.GetComponent<Player>().interact_target = null;
+    }
+}
--- Player/Controls.cs
+++ Player/Controls.cs
@@ -6,18 +6,25 @@ using UnityEngine.InputSystem;
 public class Controls : MonoBehaviour
 {
     public InputAction movement;
+    public InputAction interact;
 
     private Vector2 move_direction;
     public float speed = 5f;
 
+    public GameObject interaction_holder;
+
     public void OnEnable()
     {
         movement.Enable();
+        interact.Enable();
+        interact.performed += OnSpacePressed;
     }
 
     public void OnDisable()
     {
         movement.Disable();
+        interact.Disable();
+        interact.performed -= OnSpacePressed;
     }
 
     // Start is called before the first frame update
@@ -35,5 +42,49 @@ public class Controls : MonoBehaviour
     private void FixedUpdate()
     {
         GetComponent<Rigidbody2D>().velocity = new Vector2(move_direction.x * speed, move_direction.y * speed);
+
+        if(move_direction == new Vector2(0, 1))
+        {
+            ChangeInteractionDirection(0);
+        }
+        else if (move_direction == new Vector2(1, 0))
+        {
+            ChangeInteractionDirection(1);
+        }
+        else if (move_direction == new Vector2(0, -1))
+        {
+            ChangeInteractionDirection(2);
+        }
+        else if(move_direction == new Vector2(-1, 0))
+        {
+            ChangeInteractionDirection(3);
+        }
+    }
+
+    private void ChangeInteractionDirection(int child)
+    {
+        //Deactivate all interactors
+        for(int i = 0; i < interaction_holder.transform.childCount; i++)
+        {
+            if (interaction_holder.transform.GetChild(i).gameObject.activeInHierarchy)
+            {
+                interaction_holder.transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+
+        //Activate interactor corresponing to movedirection
+        if(!interaction_holder.transform.GetChild(child).gameObject.activeInHierarchy)
+        {
+            interaction_holder.transform.GetChild(child).gameObject.SetActive(true);
+        }
+    }
+
+    private void OnSpacePressed(InputAction.CallbackContext context)
+    {
+        Interact();
+    }
+    public void Interact()
+    {
+        if(GetComponent<Player>().interact_target != null) Debug.Log(GetComponent<Player>().interact_target.name);
     }
 }
